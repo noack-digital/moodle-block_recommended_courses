8c0a54c08d6ea0c1526665c031a7867a3d7205e5
Alexander Noack <alexander.noack@hnee.de>
2025-11-25 12:57:21 +0100

    Add comprehensive GitHub Actions CI workflow
    - Implement full Moodle plugin CI testing
    - Test matrix: Moodle 4.5/5.0 with PHP 8.0-8.3
    - Add Grunt build system with package.json and Gruntfile.js
    - Add ESLint configuration for JavaScript linting
    - Include validation steps: phplint, phpcs, phpmd, mustache, grunt
    - Configure PostgreSQL database service
    - Add PHPUnit and Behat testing
    
    Resolves GitHub Issue #1

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+/**
+ * Grunt configuration for block_recommended_courses.
+ *
+ * @package    block_recommended_courses
+ * @copyright  2025 Alexander Noack - HNEE
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+module.exports = function (grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        // ESLint for JavaScript linting
+        eslint: {
+            amd: {
+                src: ['amd/src/*.js']
+            }
+        },
+
+        // Uglify for JavaScript minification
+        uglify: {
+            options: {
+                preserveComments: 'some'
+            },
+            amd: {
+                files: [{
+                    expand: true,
+                    cwd: 'amd/src',
+                    src: ['*.js'],
+                    dest: 'amd/build',
+                    ext: '.min.js'
+                }]
+            }
+        },
+
+        // Watch for changes
+        watch: {
+            amd: {
+                files: ['amd/src/*.js'],
+                tasks: ['eslint:amd', 'uglify:amd']
+            }
+        }
+    });
+
+    // Load NPM tasks
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-eslint');
+
+    // Default task(s)
+    grunt.registerTask('default', ['eslint', 'uglify']);
+    grunt.registerTask('amd', ['eslint:amd', 'uglify:amd']);
+};
